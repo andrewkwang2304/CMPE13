e89551bcb63518c76bc51f3857c22c9c628a4ae2
akwang <akwang>
2018-01-17 20:57:19 -0800

    Part two is officially done...I hope.

--- Lab01/part2.c
+++ Lab01/part2.c
@@ -12,14 +12,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
-{
+int main(void) {
     BOARD_Init();
     /***************************************************************************************************
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
 
+    // Declare Variables
+    float fahr, celsius, kelv;
+    int lower, upper, step;
 
+    // Initialize Variables
+    lower = 0; // lower limit of temperature
+    upper = 300; // upper limit
+    step = 20; // step size
+    fahr = lower;
+
+    // Print out table
+    printf("    F     C\n");
+    while (fahr < upper) {
+        celsius = (5.0 / 9.0)*(fahr - 32.0);
+        printf("%7.1f %04.0f\n", (double) fahr, (double) celsius);
+        fahr = fahr + step;
+    }
+    printf("\n");
+
+    kelv = lower;
+
+    printf("    K     F\n");
+    // Print out table
+    while (kelv <= upper) {
+        fahr = ((kelv * (9.0 / 5.0)) - 459.67);
+        printf("%3.3f %5f\n", (double) kelv, (double) fahr);
+        kelv = kelv + step;
+    }
 
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks.
