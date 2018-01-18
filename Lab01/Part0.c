97ca31773679f5fa306c4441b933fa1f58e3cce7
akwang <akwang>
2018-01-17 20:04:17 -0800

    Done with Part 1! I love coding despite procrastinating!

---
+++ Lab01/Part0.c
@@ -0,0 +1,16 @@
+/*
+ * File:   Part0.c
+ * Author: akwang
+ *
+ * Created on January 17, 2018, 6:57 PM
+ */
+
+
+#include "xc.h"
+#include "BOARD.h"
+
+int main(void) {
+    BOARD_Init();
+    printf("Hello world\n");
+    while(1);
+}
