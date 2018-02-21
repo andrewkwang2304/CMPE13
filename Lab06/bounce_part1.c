1a14367f29ee4d0539a0467925ef22b2f36ef9d8
akwang <akwang>
2018-02-21 14:45:21 -0800

    the lights bounce back and forth, haven't implemented the switches yet though.

--- Lab06/bounce_part1.c
+++ Lab06/bounce_part1.c
@@ -9,17 +9,26 @@
 #include <plib.h>
 
 // User libraries
+#include "Leds.h"
 
 // **** Set macros and preprocessor directives ****
+#define LEFT 1
+#define RIGHT 0
+#define TRUE 1
+#define FALSE 0
 
 // **** Declare any datatypes here ****
+typedef struct {
+    uint8_t event;
+    uint8_t value;
+}TimerResult;
 
 // **** Define global, module-level, or external variables here ****
+TimerResult timerData;
 
 // **** Declare function prototypes ****
 
-int main(void)
-{
+int main(void) {
     BOARD_Init();
 
     // Configure Timer 1 using PBCLK as input. This default period will make the LEDs blink at a
@@ -35,7 +44,39 @@ int main(void)
     /***************************************************************************************************
      * Your code goes in between this comment and the following one with asterisks.
      **************************************************************************************************/
-    printf("Welcome to the Lab 6 Part 1 blank. Please remove before starting.");
+    // still needs to implement switches and stuff.
+    
+    LEDS_INIT();
+    int LED = 0x0001; // position
+    int direction = RIGHT; // sets LED direction to the right. This will help
+                           // determine the direction of the LED in if statements.
+    timerData.event = FALSE;
+    timerData.value = 0;
+    while(1) {
+        LEDS_SET(LED); // sets LED to the current one.
+        // check left side
+        if(direction == LEFT && LED == 0x0001) { 
+            direction = RIGHT;
+            LED = 0x0002;
+        }
+        // check right side
+        // 0x0080 is the hex equivalent of 0b10000000
+        else if(direction == RIGHT && LED == 0x0080) { 
+            direction = LEFT;
+            LED = 0x0040;
+        }
+        // else, just shift left or right
+        else if(timerData.event = TRUE) {
+            if(direction == LEFT) {
+                LED >>= 1;
+            } else {
+                LED <<= 1;
+            }
+            timerData.event--;
+        }
+    }
+    
+    
 
     /***************************************************************************************************
      * Your code goes in between this comment and the preceding one with asterisks
@@ -51,10 +92,12 @@ int main(void)
  * of 2, etc.). If the current counter is greater than this switch value, then the event member of a
  * module-level TimerResult struct is set to true and the value member is cleared.
  */
-void __ISR(_TIMER_1_VECTOR, IPL4AUTO) Timer1Handler(void)
-{
+void __ISR(_TIMER_1_VECTOR, IPL4AUTO) Timer1Handler(void) {
     // Clear the interrupt flag.
     INTClearFlag(INT_T1);
-
-
+    (timerData.value)++;
+    if(timerData.value > SWITCH_STATES()) {
+        timerData.event = TRUE;
+        timerData.value = 0;
+    }
 }
