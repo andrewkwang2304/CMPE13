0ddc7a19419b4974833d94ccf6676ac14a9a8952
Andrew Kwang <akwang@ucsc.edu>
2018-03-15 18:02:26 -0700

    Replace ArtificialAgent.c

--- Lab09/ArtificialAgent.c
+++ Lab09/ArtificialAgent.c
@@ -14,172 +14,190 @@
 #include "Agent.h"
 #include "Oled.h"
 
-static char maxMessage[PROTOCOL_MAX_MESSAGE_LEN];
-static AgentState agentState;
-static FieldOledTurn fieldTurn = FIELD_OLED_TURN_NONE;
-static Field playerField;
-static Field enemyField;
-static GuessData guessData, enemyGuessData;
-static NegotiationData negotiationData, enemyNegotiationData;
+static char tempMes[PROTOCOL_MAX_PAYLOAD_LEN];
+static int fieldTurn;
 
-void AgentInit(void) {
+static AgentState agentState = AGENT_STATE_GENERATE_NEG_DATA;
+static GuessData *guessData, *enemyGuessData;
+static NegotiationData *negotiationData, *enemyNegotiationData;
+static Field *playerField;
+static Field *enemyField;
+
+void AgentInit(void)
+{
+    negotiationData = malloc(sizeof (NegotiationData));
+    guessData = malloc(sizeof (GuessData));
+    enemyNegotiationData = malloc(sizeof (NegotiationData));
+    enemyGuessData = malloc(sizeof (GuessData));
+
+//    playerField = malloc(sizeof (Field));
+//    enemyField = malloc(sizeof (Field));
+
     // initialization of the field
-    FieldInit(&playerField, FIELD_POSITION_EMPTY);
-    FieldInit(&enemyField, FIELD_POSITION_UNKNOWN);
-    
-    while (!FieldAddBoat(&playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_SMALL));
-    while (!FieldAddBoat(&playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_MEDIUM));
-    while (!FieldAddBoat(&playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_LARGE));
-    while (!FieldAddBoat(&playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_HUGE));
-    
-    
+    FieldInit(playerField, FIELD_POSITION_EMPTY);
+    FieldInit(enemyField, FIELD_POSITION_UNKNOWN);
 
-    OledClear(OLED_COLOR_BLACK);
-    FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-    OledUpdate();
+    // sets Agent field empty and Enemy field unknown
+    FieldInit(playerField, FIELD_POSITION_EMPTY);
+    FieldInit(enemyField, FIELD_POSITION_UNKNOWN);
+
+    while (!FieldAddBoat(playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_SMALL));
+    while (!FieldAddBoat(playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_MEDIUM));
+    while (!FieldAddBoat(playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_LARGE));
+    while (!FieldAddBoat(playerField, rand() % 6, rand() % 10, rand() % 4, FIELD_BOAT_HUGE));
+
+    FieldOledDrawScreen(playerField, enemyField, fieldTurn);
 }
 
-int AgentRun(char in, char *outBuffer) {
-    while (1) {
-        // would we need two negotiationData and two guessData?
-        int protocolDecode = ProtocolDecode(in, &negotiationData, &guessData);
-        int enemyProtocolDecode = ProtocolDecode(in, &enemyNegotiationData, &enemyGuessData);
+int AgentRun(char in, char *outBuffer)
+{
+    int protocolDecode = 0;
+    int timeDelay = 0;
+    int messageString = 0;
 
-        switch (agentState) {
-
-            default:
-                break;
-
-
-                // Start everything up...
-            case AGENT_STATE_GENERATE_NEG_DATA:
-                ProtocolGenerateNegotiationData(&negotiationData);
-                agentState = AGENT_STATE_SEND_CHALLENGE_DATA;
-                return ProtocolEncodeChaMessage(outBuffer, &negotiationData);
-
-            case AGENT_STATE_SEND_CHALLENGE_DATA:
-                agentState = AGENT_STATE_DETERMINE_TURN_ORDER;
-                if (enemyProtocolDecode == PROTOCOL_PARSED_CHA_MESSAGE) {
-                    return ProtocolEncodeDetMessage(outBuffer, &negotiationData);
-                }
-                return 0;
-
-            case AGENT_STATE_DETERMINE_TURN_ORDER:
-                if (ProtocolValidateNegotiationData(&enemyNegotiationData)) {
-                    TurnOrder whoseTurn = ProtocolGetTurnOrder(&negotiationData, &enemyNegotiationData);
-                    switch (whoseTurn) {
-                        case TURN_ORDER_TIE:
-                            OledClear(OLED_COLOR_BLACK);
-                            OledDrawString(AGENT_ERROR_STRING_ORDERING);
-                            OledUpdate();
-                            agentState = AGENT_STATE_INVALID;
-                            break;
-                        case TURN_ORDER_DEFER:
-                            fieldTurn = FIELD_OLED_TURN_THEIRS;
-                            FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                            OledUpdate();
-                            agentState = AGENT_STATE_WAIT_FOR_GUESS;
-                            break;
-                        case TURN_ORDER_START:
-                            fieldTurn = FIELD_OLED_TURN_MINE;
-                            FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                            OledUpdate();
-                            agentState = AGENT_STATE_SEND_GUESS;
-                            break;
-                        default:
-                            OledClear(OLED_COLOR_BLACK);
-                            OledDrawString(AGENT_ERROR_STRING_NEG_DATA);
-                            OledUpdate();
-                            agentState = AGENT_STATE_INVALID;
-                            break;
-                    }
-                }
-                break;
-
-            case AGENT_STATE_INVALID:
-                return 0;
-            case AGENT_STATE_WAIT_FOR_GUESS:
-
-                if (protocolDecode == PROTOCOL_PARSED_COO_MESSAGE) {
-
-                    uint8_t agentStatus = AgentGetStatus();
-
-                    //If lives are remaining -> SEND_GUESS
-                    if (agentStatus != 0) {
-                        fieldTurn = FIELD_OLED_TURN_MINE;
-                        FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                        OledUpdate();
-                        strcpy(outBuffer, maxMessage);
-                        agentState = AGENT_STATE_SEND_GUESS;
-
-                        //If lives = 0 -> LOST
-                    } else if (agentStatus == 0) {
-                        fieldTurn = FIELD_OLED_TURN_NONE;
-                        FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                        OledUpdate();
-                        strcpy(outBuffer, maxMessage);
-                        agentState = AGENT_STATE_LOST;
-                    }
-                }
-                return ProtocolEncodeHitMessage(maxMessage, &guessData);
-                break;
-
-            case AGENT_STATE_SEND_GUESS:
-
-                //Generate valid coordinates
-                while (guessData.row >= FIELD_ROWS - 1) {
-                    guessData.row = rand();
-                }
-
-                while (guessData.col >= FIELD_COLS - 1) {
-                    guessData.col = rand();
-                }
-                guessData.hit = FieldAt(&enemyField, guessData.row, guessData.col);
-                strcpy(outBuffer, maxMessage);
-                agentState = AGENT_STATE_WAIT_FOR_HIT;
-                return ProtocolEncodeCooMessage(maxMessage, &guessData);
-                break;
-
-            case AGENT_STATE_WAIT_FOR_HIT:
-
-                if (protocolDecode == PROTOCOL_PARSED_HIT_MESSAGE) {
-
-                    //Enemy has not yet been defeated -> WAIT_FOR_GUESS(other players turn)
-                    if (AgentGetEnemyStatus() != 0) {
-                        FieldUpdateKnowledge(&enemyField, &guessData);
-                        fieldTurn = FIELD_OLED_TURN_THEIRS;
-                        FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                        OledUpdate();
-                        agentState = AGENT_STATE_WAIT_FOR_GUESS;
-
-
-                        //If enemy lives reaches 0 -> WON    
-                    } else if (AgentGetEnemyStatus() == 0) {
-                        FieldUpdateKnowledge(&enemyField, &guessData);
-                        fieldTurn = FIELD_OLED_TURN_NONE;
-                        FieldOledDrawScreen(&playerField, &enemyField, fieldTurn);
-                        OledUpdate();
-                        agentState = AGENT_STATE_WON;
-                    }
-                }
-                break;
-
-            case AGENT_STATE_LOST:
-                //Lost
-                break;
-
-            case AGENT_STATE_WON:
-                //Won
-                break;
-
-        }
+    if (in != '\0') {
+        protocolDecode = ProtocolDecode(in, enemyNegotiationData, enemyGuessData);
     }
+
+    switch (agentState) {
+        
+        // Start everything up...
+    case AGENT_STATE_GENERATE_NEG_DATA:
+        ProtocolGenerateNegotiationData(negotiationData);
+        messageString = ProtocolEncodeChaMessage(tempMes, negotiationData);
+        strncpy(outBuffer, tempMes, messageString);
+        agentState = AGENT_STATE_SEND_CHALLENGE_DATA;
+        return messageString;
+        break;
+
+    case AGENT_STATE_SEND_CHALLENGE_DATA:
+        if (protocolDecode == PROTOCOL_PARSED_CHA_MESSAGE) {
+            messageString = ProtocolEncodeDetMessage(tempMes, negotiationData);
+            strncpy(outBuffer, tempMes, messageString);
+            agentState = AGENT_STATE_DETERMINE_TURN_ORDER;
+        }
+        return messageString;
+        break;
+
+    case AGENT_STATE_DETERMINE_TURN_ORDER:
+        if (protocolDecode == PROTOCOL_PARSED_DET_MESSAGE) {
+
+            if (ProtocolValidateNegotiationData(enemyNegotiationData)) {
+                TurnOrder whoseTurn = ProtocolGetTurnOrder(negotiationData, enemyNegotiationData);
+                switch (whoseTurn) {
+                case TURN_ORDER_DEFER:
+                    fieldTurn = FIELD_OLED_TURN_THEIRS;
+                    FieldOledDrawScreen(playerField, enemyField, fieldTurn);
+                    agentState = AGENT_STATE_WAIT_FOR_GUESS;
+                    break;
+                case TURN_ORDER_START:
+                    fieldTurn = FIELD_OLED_TURN_MINE;
+                    FieldOledDrawScreen(playerField, enemyField, fieldTurn);
+                    agentState = AGENT_STATE_SEND_GUESS;
+                    break;
+                case TURN_ORDER_TIE:
+                    OledClear(OLED_COLOR_BLACK);
+                    OledDrawString(AGENT_ERROR_STRING_ORDERING);
+                    OledUpdate();
+                    agentState = AGENT_STATE_INVALID;
+                    break;
+                default:
+                    OledClear(OLED_COLOR_BLACK);
+                    OledDrawString(AGENT_ERROR_STRING_NEG_DATA);
+                    OledUpdate();
+                    agentState = AGENT_STATE_INVALID;
+                    break;
+                }
+            }
+        }
+        return messageString;
+        break;
+
+    case AGENT_STATE_SEND_GUESS:
+        //Generate valid coordinates        
+         while (guessData->row >= FIELD_ROWS - 1) {
+            guessData->row = rand();
+        }
+
+        while (guessData->col >= FIELD_COLS - 1) {
+            guessData->col = rand();
+        }
+        
+        messageString = ProtocolEncodeCooMessage(tempMes, guessData);
+        strncpy(outBuffer, tempMes, messageString);
+        agentState = AGENT_STATE_WAIT_FOR_HIT;
+
+        //Delayed thinking
+        for (timeDelay = 0; timeDelay <= BOARD_GetPBClock() / 8; timeDelay++);
+        return messageString;
+        break;
+
+    case AGENT_STATE_WAIT_FOR_GUESS:
+        if (protocolDecode == PROTOCOL_PARSED_COO_MESSAGE) {
+
+            uint8_t agentStatus = AgentGetStatus();
+            FieldRegisterEnemyAttack(playerField, enemyGuessData);
+            //If lives are remaining -> SEND_GUESS
+            if (agentStatus != 0) {
+                fieldTurn = FIELD_OLED_TURN_MINE;
+                FieldOledDrawScreen(playerField, enemyField, fieldTurn);
+                messageString = ProtocolEncodeHitMessage(tempMes, enemyGuessData);
+                strncpy(outBuffer, tempMes, messageString);
+                agentState = AGENT_STATE_SEND_GUESS;
+
+                //If lives = 0 -> LOST
+            } else if (agentStatus == 0) {
+                fieldTurn = FIELD_OLED_TURN_NONE;
+                FieldOledDrawScreen(playerField, enemyField, fieldTurn);
+                messageString = ProtocolEncodeHitMessage(tempMes, enemyGuessData);
+                strncpy(outBuffer, tempMes, messageString);
+                agentState = AGENT_STATE_LOST;
+            }
+        }
+        return messageString;
+        break;
+
+    case AGENT_STATE_WAIT_FOR_HIT:
+        if (protocolDecode == PROTOCOL_PARSED_HIT_MESSAGE) {
+
+            FieldUpdateKnowledge(enemyField, enemyGuessData);
+
+            //Enemy has not yet been defeated -> WAIT_FOR_GUESS(other players turn)
+            if (AgentGetEnemyStatus() != 0) {
+                fieldTurn = FIELD_OLED_TURN_THEIRS;
+                agentState = AGENT_STATE_WAIT_FOR_GUESS;
+
+            //If enemy lives reaches 0 -> WON    
+            } else if (AgentGetEnemyStatus() == 0) {
+                fieldTurn = FIELD_OLED_TURN_NONE;
+                agentState = AGENT_STATE_WON;
+            }
+            FieldOledDrawScreen(playerField, enemyField, fieldTurn);
+        }
+        return messageString;
+        break;
+
+    case AGENT_STATE_LOST:
+        //Lost
+        break;
+
+    case AGENT_STATE_WON:
+        //Won
+        break;
+
+    case AGENT_STATE_INVALID:
+        //break out of program. not specified where to go in lab manual
+        break;
+    }
+    return 0;
 }
 
-uint8_t AgentGetStatus(void) {
-    return FieldGetBoatStates(&playerField);
+uint8_t AgentGetStatus(void)
+{
+    return FieldGetBoatStates(playerField);
 }
 
-uint8_t AgentGetEnemyStatus(void) {
-    return FieldGetBoatStates(&enemyField);
+uint8_t AgentGetEnemyStatus(void)
+{
+    return FieldGetBoatStates(enemyField);
 }
